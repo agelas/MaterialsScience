95e4e11e42984fa17f2d3ff1a5d266dc63d893d9
Mathias <mathiaswi177@gmail.com>
2020-03-15 23:13:29 -0700

    Finished concatenating frames to original table array

--- tensileVideoMatching.m
+++ tensileVideoMatching.m
@@ -3,9 +3,9 @@
 %to be a present in the .txt file produced by the tensile tester. MATLAB
 %can only read UTF-8 encoded .txt files. but the tensile tester creates
 %UTF-16 encoded .txt files.
+
 fid = fopen('Mg1Zn0.2Ca_tensiletest2.txt','r','n','UTF-8');
 bytes = fread(fid)';
-bytes(1:15)
 
 if (bytes(1) == 255)
     bytes(1:2) = [];
@@ -19,9 +19,11 @@ else
     fclose(fid); %Nothing to do if BOM not present
 end
 
-T = readtable('Mg1Zn0.2Ca_tensiletest2.txt')
+T = readtable('Mg1Zn0.2Ca_tensiletest2.txt'); %
 
 %%
+%This section breaks the video up into individual frames, and creates a 
+%video object with accessible properties.
 
 v = VideoReader('Mg1Zn0.2Ca_Ingot2_TestVid.avi');
 
@@ -32,5 +34,28 @@ while hasFrame(v)
     numFramesIndex = numFramesIndex + 1;
     imArray{numFramesIndex,1} = F;
 end
+
+%%
+%This section matches the frames to the times in the video
+failTime = 81 * 5; %get into 0.2 of a second
+frameFail = 6 * failTime;
+
+index = find(T{:,:} == 81); %find table index of failure time
+Frames = cell(size(T,1),1);
+
+for i = index:-1:1 
+    frameMatrix = [frameFail-5 frameFail-4 frameFail-3 frameFail-2 frameFail-1 frameFail];
+    Frames{i,1} = int2str(frameMatrix);
+    if frameFail > 0
+        frameFail = frameFail - 6;
+    else
+        break;
+    end
+end
+
+frameTable = cell2table(Frames, 'VariableNames', {'Frames'});
+
+fullTable = [T frameTable]
+
 
 
