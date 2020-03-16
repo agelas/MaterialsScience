bd08ded3db7e05356886bb16daf57c5f808571d5
Mathias <mathiaswi177@gmail.com>
2020-03-15 21:13:15 -0400

    Added comments to make more understandable

--- recrystallization.m
+++ recrystallization.m
@@ -1,14 +1,13 @@
 clear all
 clc
 
-file='ECAE_MgZnCa_C_5x_6.jpg';
+file='ECAE_MgZnCa_C_5x_6.jpg'; %file to be loaded
 fileImport=imread(file);
 binary= im2bw(fileImport,0.5);
-%binary = imbinarize(fileImport);
 
 %%
 figure(1);
-clean=bwmorph(binary,'majority',70);
+clean=bwmorph(binary,'majority',70); 
 clean=bwmorph(clean, 'spur', 70);
 structF=strel('diamond',5);
 clean=imclose(clean, structF);
@@ -20,5 +19,6 @@ subplot(2,2,2); imshow(invertedImage);
 sumWhite = sum(clean(:) > 0);
 [rows, columns] = size(clean);
 areaRecrystallized = sumWhite / (rows*columns);
-percentAreaRecrystallized = 100 - areaRecrystallized*100
+percentAreaRecrystallized = 100 - areaRecrystallized*100;
+fprintf("Percent Recrystallized Area: %f\n", percentAreaRecrystallized);
 areaBlack = 1 - areaRecrystallized;
