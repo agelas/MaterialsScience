95ef09252f34701375667e5a5cd9ceb4625142ae
Mathias <mathiaswi177@gmail.com>
2020-04-05 22:01:18 -0700

    Modified so that you can control if it plots the Heat of Formation

--- plotHeatFormationDiff.m
+++ plotHeatFormationDiff.m
@@ -2,7 +2,7 @@
 %Calls calculateHIron to get the heat of formation at a specific
 %temperature. Uses that return value to find diffeence between that and
 %Heat of formation of 298K. Plots difference. 
-function plotHeatFormationDiff(temperature)
+function hTemp = plotHeatFormationDiff(temperature, toPlot)
     tempRange = 298:temperature;
     hTemp = zeros(1,max(size(tempRange)));
     
@@ -10,10 +10,12 @@ function plotHeatFormationDiff(temperature)
         hTemp(i) = calculateHIron(tempRange(i),0) - 25.09;
     end
     
-    plot(tempRange, hTemp);
-    xlabel('Temperature (K)')
-    ylabel('HT - H298 (J/mol)')
-    title('HT - H298 (J/mol) vs Temperature (K) for Iron');
+    if(toPlot == 1)
+        plot(tempRange, hTemp);
+        xlabel('Temperature (K)')
+        ylabel('HT - H298 (J/mol)')
+        title('HT - H298 (J/mol) vs Temperature (K) for Iron');
+    end
     
     fprintf("Heat at %d is %f\n", temperature, hTemp(end));
 end
